1d851979e7140723098720e44b5d3279c12af006
matheusvdes007 <mvsousa5@gmail.com>
2021-02-12 18:31:25 -0300

    Lançando o projeto no git

---
+++ bot/bot.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+import discord
+from discord.ext import commands
+from discord.flags import Intents
+
+class MusicBot(commands.Bot):
+    def __init__(self):
+        self._cogs = [p.stem for p in Path(".").glob("./bot/cogs/*.py")]
+        super().__init__(command_prefix=["!", "."], case_insensitive=True)
+
+    
+    def setup(self):
+        print("Running setup...")
+        
+        for cog in self._cogs:
+            self.load_extension(f"bot.cogs.{cog}")
+            print(f"Loaded '{cog}' cog.")
+        
+        print("Setup Complete.")        
+    
+    # essa função é chamada no launcher.py
+    def run(self):
+        self.setup()
+
+        with open("data/token.0", "r", encoding="utf-8") as f:
+            TOKEN = f.read()
+
+        print("Running bot...")
+        # essa função run é propria do python
+        super().run(TOKEN, reconnect=True)
+
+    async def shutdown(self):
+        print("Closing connection to Discord...")
+        await super().close()
+    
+    async def close(self):
+        print("Closing on keyboard interrupt...")
+        await self.shutdown()
+
+    async def on_connect(self):
+        print(f"Connected to Discord (latency : {self.latency*1000:,.0f} ms).")
+
+    async def on_resumed(self):
+        print(f"Bot resumed.")
+
+    async def on_disconnect(self):
+        print(f"Bot disconnected.")
+
+    async def on_ready(self):
+        self.client_id = (await self.application_info()).id
+        print("Bot ready.")
+
+  #  async def on_error(self, err, *args, **kwargs):
+  #      raise
+
+  #  async def on_command_error(self, ctx, exc):
+  #      raise getattr(exc, "original", exc)
+
+
+    async def command_prefix(self, bot, msg):
+        return commands.when_mentioned_or(["!", "."])(bot,msg)
+
+    async def process_commands(self, msg):
+        ctx = await self.get_context(msg, cls=commands.Context)
+
+        if ctx.command is not None:
+            await self.invoke(ctx)
+
+    async def on_message(self, message):
+        if not message.author.bot:
+            await self.process_commands(message)
---
+++ bot/cogs/weather.py
@@ -0,0 +1,53 @@
+import requests
+import datetime as dt
+import typing as t
+import discord
+from discord.ext import commands
+import json
+
+with open("../../data/api_key.0", "r", encoding="utf-8") as f:
+    API_KEY = f.read()
+
+class Weather(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(name="search")
+    async def search_command(self, ctx):
+        msg = ctx.message.content.split()
+        place = '+'.join(msg[1:])
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={place}&appid={API_KEY}&lang=pt"
+        response = requests.request("GET", url).json()
+        temp = str("%.2f" % (float(response["main"]["temp"]) - 273.0))
+        sens = str("%.2f" % (float(response["main"]["feels_like"]) - 273.0))
+        temp_min = str("%.2f" % (float(response["main"]["temp_min"]) - 273.0))
+        temp_max = str("%.2f" % (float(response["main"]["temp_max"]) - 273.0))
+        pressure = str(response["main"]["pressure"])
+        humidity = str(response["main"]["humidity"])
+        new_place = ' '.join(msg[1:])
+        newmsg = "Cidade: " + new_place + "\nClima: " + response["weather"][0]["description"] + "\nTemperatura: " + temp + "ºC \nSensação Térmica: " + sens + "ºC \nTemperatura mínima: " + temp_min + "ºC \nTemperatura máxima: " + temp_max + "ºC \nPressão: " + pressure + "\nUmidade: " + humidity
+        await ctx.send(newmsg)
+
+    @commands.command(name="state")
+    async def state_command(self, ctx):
+        await ctx.send("Digite a cidade desejada: ")
+        city_msg = await self.bot.wait_for('message')
+        tmp = city_msg.content.split()
+        cidade = '+'.join(tmp)
+        await ctx.send("Digite o estado desejado: ")
+        state_msg = await self.bot.wait_for('message')
+        tmp = state_msg.content.split()
+        estado = '+'.join(tmp)
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade},{estado}&appid={API_KEY}&lang=pt"
+        response = requests.request("GET", url).json()
+        temp = str("%.2f" % (float(response["main"]["temp"]) - 273.0))
+        sens = str("%.2f" % (float(response["main"]["feels_like"]) - 273.0))
+        temp_min = str("%.2f" % (float(response["main"]["temp_min"]) - 273.0))
+        temp_max = str("%.2f" % (float(response["main"]["temp_max"]) - 273.0))
+        pressure = str(response["main"]["pressure"])
+        humidity = str(response["main"]["humidity"])
+        newmsg = "Cidade: " + city_msg.content + ", " + state_msg.content + "\nClima: " + response["weather"][0]["description"] + "\nTemperatura: " + temp + "ºC \nSensação Térmica: " + sens + "ºC \nTemperatura mínima: " + temp_min + "ºC \nTemperatura máxima: " + temp_max + "ºC \nPressão: " + pressure + "\nUmidade: " + humidity
+        await ctx.send(newmsg)
+
+def setup(bot):
+    bot.add_cog(Weather(bot))
